3322dd4fadb571b117c3ab6cd3432ba9b04a5441
Carlos Habiague <84341443+Carloshha@users.noreply.github.com>
2021-09-21 21:52:21 -0300

    Add files via upload

---
+++ contactoyfooter.php
@@ -0,0 +1,77 @@
+<section class="contacto-section" id="contacto">
+      <div class="container">
+        <div class="row mx-auto text-center">
+          <div class="col-lg-8 mx-auto">
+            <h3 class="mb-4">contacto</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-8 col-12 mx-auto">
+            <form action ="contacto.php " method="post">
+              <div class="row mb-3">
+                <div class="col">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Nombre"
+                    id="name"
+                    name="nombre"
+                  />
+                </div>
+                <div class="col">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Apellido"
+                    id="lastname"
+                    name="apellido"
+                  />
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Teléfono"
+                    id="phone"
+                    name="telefono"
+                  />
+                </div>
+                <div class="col">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Email"
+                    id="email"
+                    name="email"
+                  />
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col">
+                  <textarea
+                    type="text"
+                    class="form-control"
+                    placeholder="Mensaje"
+                    id="message"
+                    name="mensaje"
+                  ></textarea>
+                </div>
+              </div>
+              <div class="row mb-3">
+                <div class="col"></div>
+                <div class="col">
+                  <button type="submit" class="btn btn-enviar">Enviar</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <footer class="footer small text-center">
+        <div class="container">
+          Plantilla web didáctica - Potrero Digital 2021
+        </div>
+      </footer>
+    </section>
